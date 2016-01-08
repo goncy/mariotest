973e283e271a5a9a0530f5c976fd77a24d26ba98
gonzalo.pozzo <gonzalo.pozzo@etermax.com>
2016-01-08 14:24:23 -0300

    Barra de dificultad

--- src/api/champions.js
+++ src/api/champions.js
@@ -1,7 +1,7 @@
 //ng-data
 module.exports = new Backbone.Collection([
-  {name: "Ahri", linea: "mid", sexo:"femenino"},
-  {name: "Diana", linea: "mid", sexo:"femenino"},
-  {name: "Akali", linea: "mid", sexo:"femenino"},
-  {name: "Orianna", linea: "mid", sexo:"femenino"}
+  {name: "Ahri", linea: "mid", sexo:"femenino", dificultad:"80", strongAgains:['Orianna'], weakAgainst:['Diana', 'Akali']},
+  {name: "Diana", linea: "mid", sexo:"femenino", dificultad:"90", strongAgains:['Ahri', 'Akali'], weakAgainst:['Orianna']},
+  {name: "Akali", linea: "mid", sexo:"femenino", dificultad:"60", strongAgains:['Orianna'], weakAgainst:['Diana', 'Ahri']},
+  {name: "Orianna", linea: "mid", sexo:"femenino", dificultad:"100", strongAgains:['Diana', 'Akali'], weakAgainst:['Ahri']}
 ]);
