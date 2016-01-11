4016bfbdf160512950a9256e06eb3bef7480c6e1
gonzalo.pozzo <gonzalo.pozzo@etermax.com>
2016-01-11 11:05:11 -0300

    Arreglado router

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,13 @@ var browserify = require('gulp-browserify');
 var hbsfy = require('hbsfy');
 var rename = require('gulp-rename')
 var gulp = require('gulp');
+var rm = require('del');
 
-gulp.task('default', function() {
+gulp.task('clean', function(){
+   return rm('./bundle.js');
+});
+
+gulp.task('default', ['clean'], function() {
   return gulp.src('./src/index.js')
     .pipe(browserify())
     .pipe(rename('bundle.js'))
--- src/index.js
+++ src/index.js
@@ -1,16 +1,46 @@
 //Vendors
-_ = require('underscore');
-Backbone = require('backbone');
-Marionette = require('backbone.marionette');
-Marionette.$ = require('jquery');
+window._ = require('underscore');
+window.Backbone = require('backbone');
+window.Marionette = require('backbone.marionette');
+window.Marionette.$ = require('jquery');
 
-var championPage = require('./views/championPage'),
-    champions = require('./api/champions');
+var ChampionPage = require('./views/championPage'),
+  champions = require('./api/champions');
 
-//ng-call
-var chp = new championPage({
-  el: "body",
-  collection: champions
+var app = new Marionette.Application();
+
+var ChampionsCtrl = Marionette.Object.extend({
+  initialize: function () {
+    console.log("Arranca ctrl");
+  },
+  homePage: function () {
+    console.log("Arranca home");
+  },
+  championList: function () {
+    console.log('Arranca list');
+    (new ChampionPage({
+      el: "body",
+      collection: champions
+    })).render();
+  },
+  idChampionList: function (id) {
+    console.log('ID: ' + id);
+  }
 });
 
-chp.render();
+var Router = Marionette.AppRouter.extend({
+  appRoutes: {
+    "": "homePage",
+    "list": "championList",
+    "list/:champ": "idChampionList"
+  },
+  controller: new ChampionsCtrl()
+});
+
+app.on('start', function () {
+  app.router = new Router();
+  Backbone.history.start();
+  console.log("Arranca");
+});
+
+app.start();
